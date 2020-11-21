ae3c8e26897d4b3606683c4d568a559c47a910fe
mrwise1 <sahinmustafaenes1@gmail.com>
2020-11-21 13:04:28 -0500

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git Last Day");
         System.out.println("B21 is exited");
+        //HSK app
     }
 }
